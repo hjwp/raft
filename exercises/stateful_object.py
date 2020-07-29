075f8562d0455dd7cee58f0bf9d072eb64aaa1c7
Harry <hjwp2@cantab.net>
2020-07-29 23:05:20 +0100

    ok here's your OO solution. with type hints sorry but not too many

--- exercises/stateful_object.py
+++ exercises/stateful_object.py
@@ -1,35 +1,77 @@
+# pylint: disable=no-self-use,redefined-builtin
+from __future__ import annotations
+from typing import Protocol
 from socket import socket, AF_INET, SOCK_STREAM
 
 class ConnectionError(Exception):
     pass
 
-class Connection:
-    def __init__(self, host, port):
-        self.state = 'CLOSED'
-        self.host = host
-        self.port = port
-        self.sock = None
 
-    def connect(self):
-        if self.state != 'CLOSED':
-            raise ConnectionError("Not closed")
-        self.sock = socket(AF_INET, SOCK_STREAM)
-        self.sock.connect((self.host, self.port))
-        self.state = 'OPEN'
+class Socket(Protocol):
+
+    def connect(self, host: str, port: int) -> Socket:
+        ...
+
+    def send(self, msg: bytes) -> int:
+        ...
+
+    def recv(self, maxbytes: int) -> bytes:
+        ...
+
+    def close(self) -> None:
+        ...
+
+
+class ClosedSocket:
+
+    def connect(self, host, port):
+        sock = socket(AF_INET, SOCK_STREAM)
+        sock.connect((host, port))
+        return OpenSocket(sock)
 
     def send(self, msg):
-        if self.state != 'OPEN':
-            raise ConnectionError("Not open")
+        raise ConnectionError("Not open")
+
+    def recv(self, maxbytes):
+        raise ConnectionError("Not open")
+
+    def close(self):
+        raise ConnectionError("Not open")
+
+
+class OpenSocket:
+
+    def __init__(self, sock: socket):
+        self._sock = sock
+
+    def connect(self, host, port):
+        raise ConnectionError("Not closed")
+
+    def send(self, msg):
+        return self._sock.send(msg)
+
+    def recv(self, maxbytes):
+        return self._sock.recv(maxbytes)
+
+    def close(self):
+        self._sock.close()
+
+
+class Connection:
+
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = ClosedSocket()
+
+    def connect(self):
+        self.sock = self.sock.connect(self.host, self.port)
+
+    def send(self, msg):
         return self.sock.send(msg)
 
     def recv(self, maxbytes):
-        if self.state != 'OPEN':
-            raise ConnectionError("Not open")
         return self.sock.recv(maxbytes)
 
     def close(self):
-        if self.state != 'OPEN':
-            raise ConnectionError("Not open")
         self.sock.close()
-        self.state = 'CLOSED'
-
