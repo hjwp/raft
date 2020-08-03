0646e4052ac2da5c03b4d76aa7b6e825ec2ffb02
Harry <hjwp2@cantab.net>
2020-08-03 21:19:30 +0100

    wip on traffic lights

--- test_traffic_light.py
+++ test_traffic_light.py
@@ -1,3 +1,4 @@
+import time
 from trafficlight import TrafficLight
 
 
@@ -5,3 +6,53 @@ def test_start_state_is_arbitrarily_ns_green():
     l = TrafficLight()
     assert l.ns == '🟢⚫⚫'
     assert l.ew == '⚫⚫🔴'
+
+
+def test_full_cyclye():
+    l = TrafficLight()
+    assert l.ns == '🟢⚫⚫'
+    assert l.ew == '⚫⚫🔴'
+    l.next()
+    assert l.ns == '⚫🟡⚫'
+    assert l.ew == '⚫⚫🔴'
+    l.next()
+    assert l.ns == '⚫⚫🔴'
+    assert l.ew == '🟢⚫⚫'
+    l.next()
+    assert l.ns == '⚫⚫🔴'
+    assert l.ew == '⚫🟡⚫'
+    l.next()
+    assert l.ns == '🟢⚫⚫'
+    assert l.ew == '⚫⚫🔴'
+
+
+def test_time_til_next():
+    l = TrafficLight()
+    assert l.time_til_next == 30
+    l.next()
+    assert l.time_til_next == 5
+    l.next()
+    assert l.time_til_next == 60
+    l.next()
+    assert l.time_til_next == 5
+    l.next()
+    assert l.time_til_next == 30
+
+
+# def test_is_time_yet():
+#     start = time.time()
+#     l = TrafficLight()
+#     assert l.is_time_yet(time.time()) is False
+#     assert l.is_time_yet(start + 28) is False
+#     assert l.is_time_yet(start + 31) is True
+
+
+def test_push_button():
+    l = TrafficLight()
+    l.next()
+    l.next()
+    assert l.ns == '⚫⚫🔴'
+    assert l.ew == '🟢⚫⚫'
+    assert l.time_til_next == 60
+    l.push_button()
+    assert l.time_til_next == 30
--- trafficlight.py
+++ trafficlight.py
@@ -1,5 +1,37 @@
-class TrafficLight:
+import time
+from dataclasses import dataclass
+from itertools import cycle
 
-    def __init__(self):
-        self.ns = '🟢⚫⚫'
-        self.ew = '⚫⚫🔴'
+GREEN = '🟢⚫⚫'
+AMBER = '⚫🟡⚫'
+RED = '⚫⚫🔴'
+
+STATE_CYCLE = (
+    (GREEN, RED, 30),
+    (AMBER, RED, 5),
+    (RED, GREEN, 60),
+    (RED, AMBER, 5),
+    (GREEN, RED, 30),
+)
+
+
+@dataclass
+class TrafficLight:
+    ns: str
+    ew: str
+    time_til_next: int
+
+    def __init__(self) -> None:
+        self._cycle = cycle(STATE_CYCLE)
+        self.next()
+
+    def next(self) -> None:
+        self._last_cycle_time = time.time()
+        self.ns, self.ew, self.time_til_next = next(self._cycle)
+
+    def tick(self) -> None:
+        self.time_til_next -= 1
+
+    def push_button(self) -> None:
+        if self.ns == RED and self.ew == GREEN:
+            pass
