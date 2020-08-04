e65fb8ae70ea73cd8da8625760854b8fd0aa38d2
Harry <hjwp2@cantab.net>
2020-08-04 17:06:28 +0100

    progress on log

--- src/raft/log.py
+++ src/raft/log.py
@@ -1,32 +1,55 @@
-from typing import List
+from typing import List, Optional
 from dataclasses import dataclass
-
-@dataclass
-class Command:
-    key: str
-    val: str
-
 
 @dataclass
 class Entry:
     term: int
-    committed: bool
-    command: Command
+    command: str
 
 
 class InMemoryLog:
 
-    def __init__(self, log: List[Entry]):
+    def __init__(self, log: List[Entry]) -> None:
         self._log = log
+
+    @property
+    def lastLogIndex(self):
+        """1-based"""
+        return len(self._log)
+
+    def _has_entry_at(self, index: int) -> bool:
+        """1-based"""
+        return index <= len(self._log)
+
+    def _entry_at(self, index: int) -> Entry:
+        """return 1-based index entry"""
+        return self._log[index - 1]
 
     def add_entry(
         self,
-        entry: Command,
-        prevLogIndex: int,
+        entry: Entry,
+        prevLogIndex: int,  # 1-based
         prevLogTerm: int,
-        leaderCommit: int
-    ):
-        ...
+        leaderCommit: int,  # 1-based, ignored for now
+    ) -> bool:
+        if prevLogIndex > self.lastLogIndex:
+            return False
+        new_index = prevLogIndex + 1
+        if prevLogIndex == 0:
+            # TODO: what if it shouldn't be?
+            self._log.append(entry)
+            return True
 
-    def read(self):
+        if self._has_entry_at(new_index) and self._entry_at(new_index) == entry:
+            return True
+
+        prev_entry = self._entry_at(prevLogIndex)
+        if prev_entry.term != prevLogTerm:
+            return False
+
+        self._log.append(entry)
+        return True
+
+
+    def read(self) -> List[Entry]:
         return self._log
--- tests/unit/test_log.py
+++ tests/unit/test_log.py
@@ -1,8 +1,73 @@
-from raft.log import InMemoryLog, Entry, Command
+from raft.log import InMemoryLog, Entry
+
 
 def test_add_entry_happy_path():
-    old_entry = Entry(term=1, committed=False, command=Command('foo', '1'))
+    old_entry = Entry(term=1, command="foo=1")
     log = InMemoryLog([old_entry])
-    new_entry = Entry(term=1, committed=False, command=Command('foo', '2'))
-    log.add_entry(new_entry, prevLogIndex=1, prevLogTerm=1, leaderCommit=0)
+    new_entry = Entry(term=1, command="foo=2")
+    result = log.add_entry(new_entry, prevLogIndex=1, prevLogTerm=1, leaderCommit=0)
     assert log.read()[-1] == new_entry
+    assert result is True
+
+
+def test_add_first_entry():
+    log = InMemoryLog([])
+    new_entry = Entry(term=1, command="foo=2")
+    log.add_entry(new_entry, prevLogIndex=0, prevLogTerm=0, leaderCommit=0)
+    assert log.read() == [new_entry]
+
+
+def test_idempotent_at_end():
+    old_entry = Entry(term=1, command="foo=1")
+    log = InMemoryLog([old_entry])
+    new_entry = Entry(term=1, command="foo=2")
+    log.add_entry(new_entry, prevLogIndex=1, prevLogTerm=1, leaderCommit=0)
+    result = log.add_entry(new_entry, prevLogIndex=1, prevLogTerm=1, leaderCommit=0)
+    assert log.read() == [old_entry, new_entry]
+    assert result is True
+
+
+def test_cannot_add_past_end():
+    old_entry = Entry(term=1, command="foo=1")
+    log = InMemoryLog([old_entry])
+    new_entry = Entry(term=1, command="foo=2")
+    result = log.add_entry(new_entry, prevLogIndex=2, prevLogTerm=1, leaderCommit=0)
+    assert log.read() == [old_entry]
+    assert result is False
+
+
+def test_cannot_add_if_prevLogTerm_does_not_Match():
+    old_entry = Entry(term=1, command="foo=1")
+    log = InMemoryLog([old_entry])
+    new_entry = Entry(term=1, command="foo=2")
+    result = log.add_entry(new_entry, prevLogIndex=1, prevLogTerm=2, leaderCommit=0)
+    assert log.read() == [old_entry]
+    assert result is False
+
+def test_idempotent_at_end():
+    old_entry = Entry(term=1, command="foo=1")
+    log = InMemoryLog([old_entry])
+    new_entry = Entry(term=1, command="foo=2")
+    log.add_entry(new_entry, prevLogIndex=1, prevLogTerm=1, leaderCommit=0)
+    result = log.add_entry(new_entry, prevLogIndex=1, prevLogTerm=1, leaderCommit=0)
+    assert log.read() == [old_entry, new_entry]
+    assert result is True
+
+
+def test_cannot_add_past_end():
+    old_entry = Entry(term=1, command="foo=1")
+    log = InMemoryLog([old_entry])
+    new_entry = Entry(term=1, command="foo=2")
+    result = log.add_entry(new_entry, prevLogIndex=2, prevLogTerm=1, leaderCommit=0)
+    assert log.read() == [old_entry]
+    assert result is False
+
+
+def test_cannot_add_if_prevLogTerm_does_not_match():
+    old_entry = Entry(term=1, command="foo=1")
+    log = InMemoryLog([old_entry])
+    new_entry = Entry(term=1, command="foo=2")
+    result = log.add_entry(new_entry, prevLogIndex=1, prevLogTerm=2, leaderCommit=0)
+    assert log.read() == [old_entry]
+    assert result is False
+
