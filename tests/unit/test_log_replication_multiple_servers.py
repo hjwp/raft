bb55cc500ea9789c6db2424ff5fefb6a040bf22a
Harry <hjwp2@cantab.net>
2020-08-04 23:57:47 +0100

    i.. think... its working.  with clock tick i can test replication

--- tests/unit/test_log_replication_multiple_servers.py
+++ tests/unit/test_log_replication_multiple_servers.py
@@ -1,25 +1,28 @@
-from typing import Dict, List
-from collections import defaultdict
+from typing import List
 from raft.adapters.run_server import clock_tick
 from raft.log import InMemoryLog
 from raft.messages import Message, ClientSetCommand
 from raft.server import Leader, Follower
 
 class FakeRaftNetwork:
-    def __init__(self, me: str, peers: List[str]):
+    def __init__(self, me: str, messages: List[Message]):
         self.me = me
-        self.peers = peers
-        self.messages = []  # type: List[Message]
+        self.messages = messages
 
     def get_messages(self) -> List[Message]:
-        return [m for m in self.messages if m.to == self.me]
+        """retrieve my messages, and take them out of the network"""
+        mine = [m for m in self.messages if m.to == self.me]
+        for m in mine:
+            self.messages.remove(m)
+        return mine
 
     def dispatch(self, msg: Message) -> None:
+        """put the message into the network"""
         self.messages.append(msg)
 
 
-def test_replication():
-    peers = ("S2",)
+def test_replication_one_server():
+    peers = ["S2"]
 
     leader_log = InMemoryLog([])
     leader = Leader(
@@ -29,7 +32,38 @@ def test_replication():
     follower_log = InMemoryLog([])
     follower = Follower(name="S2", log=follower_log, currentTerm=1, votedFor=None)
 
-    leader.handle_message(Message(frm='client.id', to='S1', cmd=ClientSetCommand(
-        'foo=1'
-    )))
+    client_set = Message(frm='client.id', to='S1', cmd=ClientSetCommand('foo=1'))
 
+    messages = []  # type: List[Message]
+    lnet = FakeRaftNetwork(me="S1", messages=messages)
+    fnet = FakeRaftNetwork(me="S2", messages=messages)
+    messages.append(client_set)
+    clock_tick(leader, lnet)
+    clock_tick(follower, fnet)
+    assert follower.log.read()[-1].cmd == client_set.cmd.cmd
+
+def test_replication_multiple_servers():
+    peers = ("S2", "S3")
+
+    leader_log = InMemoryLog([])
+    leader = Leader(
+        name="S1", peers=peers, log=leader_log, currentTerm=1, votedFor=None
+    )
+
+    f1_log = InMemoryLog([])
+    f1 = Follower(name="S2", log=f1_log, currentTerm=1, votedFor=None)
+    f2_log = InMemoryLog([])
+    f2 = Follower(name="S3", log=f1_log, currentTerm=1, votedFor=None)
+
+    client_set = Message(frm='client.id', to='S1', cmd=ClientSetCommand('foo=1'))
+
+    messages = []  # type: List[Message]
+    lnet = FakeRaftNetwork(me="S1", messages=messages)
+    f1net = FakeRaftNetwork(me="S2", messages=messages)
+    f2net = FakeRaftNetwork(me="S3", messages=messages)
+    messages.append(client_set)
+    clock_tick(leader, lnet)
+    clock_tick(f1, f1net)
+    clock_tick(f2, f2net)
+    assert f1.log.read()[-1].cmd == client_set.cmd.cmd
+    assert f2.log.read()[-1].cmd == client_set.cmd.cmd
